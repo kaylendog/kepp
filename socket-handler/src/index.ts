6940149ca41bcd76e47a5aa7575f0d7289079492
skye <actuallyori@gmail.com>
2020-01-17 23:02:57 +0000

    Remove the token that's obviously in public display

--- socket-handler/src/index.ts
+++ socket-handler/src/index.ts
@@ -1,6 +1,6 @@
 import { Manager } from "./structures/Manager";
 
 const manager = new Manager({
-	token: 'NDMwNDU2MDY4NzI4OTQ2NzA4.XhsLdg.sMNwHh5na55DSLopge9OelQteW8'
+	token: 'the token that was here got invalidated lul'
 });
 manager.startShards();
