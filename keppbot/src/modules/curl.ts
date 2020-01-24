c99d702ae908d1982de6bf1d87b6114b3dac2e97
sparklfox <actuallyori@gmail.com>
2020-01-24 00:35:07 +0000

    Tweaks to module

--- keppbot/src/modules/curl.ts
+++ keppbot/src/modules/curl.ts
@@ -1,10 +1,11 @@
 import { command, Context, Module } from '../../../packages/snepsek/src/';
 
 export class Curl extends Module {
-	@command({ disabled: false, inhibitors: [] })
+	@command()
 	async curl(ctx: Context) {}
 
 	async moduleDidInit() {
 		this.logger.info(`Curl module initialized.`);
 	}
+	async uwu() {}
 }
