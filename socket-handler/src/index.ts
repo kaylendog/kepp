8143f2ff514691d612c9dcd97dc06548ba4c1080
sparklfox <actuallyori@gmail.com>
2020-01-25 09:37:41 +0000

    Copy env folder because I'm an idiot

--- socket-handler/src/index.ts
+++ socket-handler/src/index.ts
@@ -1,6 +1,7 @@
+import { env } from './env';
 import { Manager } from './Manager';
 
 const manager = new Manager({
-	token: 'NDMwNDU2MDY4NzI4OTQ2NzA4.Xiot8Q.lOE4choFjpR-sBAHhT0w-cWcre8'
+	token: env.TOKEN
 });
 manager.startShards();
